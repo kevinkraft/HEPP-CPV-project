e4b0a30210ca76ed828c863a485c32ba1ba2f6d1
kevinkraft <kevin.maguire.2@ucdconnect.ie>
2013-10-17 11:40:57 +0100

    made a program to make it easier to commit pdflatex junk to github

---
+++ Tex/gitrun.sh
@@ -0,0 +1,12 @@
+[ -z $BASH ] || shopt -s expand_aliases
+alias BEGINCOMMENT="if [ ]; then"
+alias ENDCOMMENT="fi"
+BEGINCOMMENT
+echo "Program to push changes to git after running pdflatex"
+echo "This is helpful as there is lots of junk made by pdflatex and this program will commit them all so you don't have to"
+echo "note you still have to add a commit for main.tex"
+echo "Run with: bash gitrun.sh "
+ENDCOMMENT
+
+
+
