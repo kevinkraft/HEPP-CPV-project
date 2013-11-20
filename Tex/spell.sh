e31c6ab8df21d1734a72d1fbac188f56fc5d2b7f
kevinkraft <kevin.maguire.2@ucdconnect.ie>
2013-11-20 11:40:57 +0000

    started spell checking, made a spell checker bash

---
+++ Tex/spell.sh
@@ -0,0 +1,8 @@
+aspell --lang=en_GB -t -c main.tex
+aspell --lang=en_GB -t -c Introduction.tex
+aspell --lang=en_GB -t -c P_C_CPT.tex
+aspell --lang=en_GB -t -c CKM-Mechanism.tex
+aspell --lang=en_GB -t -c CPV_in_Kaon_System.tex
+aspell --lang=en_GB -t -c Bphysics.tex
+aspell --lang=en_GB -t -c Deon_CPV.tex
+aspell --lang=en_GB -t -c SCVPsect.tex
